c9b29cde16e3e7d0466e6727fa7bba6973704f59
wanghaoxu <wang.haoxu@bitget.com>
2022-10-12 10:58:04 +0800

    feat(core): add GVR switch

--- 1.0/src/com/henry/jenkins/pipeline/HelmUtils.groovy
+++ 1.0/src/com/henry/jenkins/pipeline/HelmUtils.groovy
@@ -20,6 +20,18 @@ def genYaml() {
         helm template ${SERVICE_NAME} ${HELM_WORKSPACE} -f ${HELM_WORKSPACE}/override.yaml --output-dir ${SERVICE_NAME}
         
         sed -i -E 's#(name..).*#\\1${APPLICATION_IMAGE_ECR}/${SERVICE_NAME}#' ./${SERVICE_NAME}/base/kustomization.yaml
+
+        if [ ${params.GVR} == "deployment" ]; then
+            sed -i -E  \
+                 -e 's@(.*)statefulset(.*)@#\\1statefulset\\2@' \ 
+                 -e 's@(.*)service-headless(.*)@#\\1service-headless\\2@'  \
+            ./${SERVICE_NAME}/base/kustomization.yaml
+        else 
+            sed -i -E  \
+                 -e 's@(.*)deployment(.*)@#\\1deployment\\2@' \ 
+                 -e 's@(.*)service.yaml@#\\1service.yaml@'  \
+            ./${SERVICE_NAME}/base/kustomization.yaml
+        fi
     """
 }
 
