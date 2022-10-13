84946c0437e715d653e5ae22472d60363cddfd80
wanghaoxu <wang.haoxu@bitget.com>
2022-10-13 17:47:23 +0800

    feat(build): add build feature

--- 1.0/vars/java.groovy
+++ 1.0/vars/java.groovy
@@ -1,3 +1,10 @@
 import com.henry.jenkins.pipeline.java.JavaUtils
 
-def javautils = new JavaUtils()
+class Global {
+    static Object javautils = new JavaUtils()
+}
+
+
+def build() {
+    Global.javautils.javaBuild()
+}
