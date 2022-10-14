cc08338fc00f71811c582068fbd29e9404733bda
wwwhaoxu <53362740+wwwhaoxu@users.noreply.github.com>
2022-10-14 22:02:06 +0800

    Update JavaUtils.groovy

--- 1.0/src/com/henry/jenkins/pipeline/java/JavaUtils.groovy
+++ 1.0/src/com/henry/jenkins/pipeline/java/JavaUtils.groovy
@@ -64,18 +64,16 @@ def deploy() {
         }
         
     } 
+    Global.common.info("hello world")
     
-    for(int count = 0; count < 3; count++) {
-                sleep(5)
-                Global.common.info "hello world"
-    }
+    
 }
 
 def checkStatus() {
     for(int count = 0; count < 60; count++) {
         sh 'sleep 5'
         Global.common.checkPodRun("sts")
-    Global.common.info("hello world")
+        Global.common.info("hello world")
     }
 }
 
