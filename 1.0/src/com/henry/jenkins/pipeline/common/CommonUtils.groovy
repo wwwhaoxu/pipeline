ad1f69029aa9293d1166f2a6975391c601e4ea06
wwwhaoxu <53362740+wwwhaoxu@users.noreply.github.com>
2022-10-14 22:45:16 +0800

    Update CommonUtils.groovy

--- 1.0/src/com/henry/jenkins/pipeline/common/CommonUtils.groovy
+++ 1.0/src/com/henry/jenkins/pipeline/common/CommonUtils.groovy
@@ -43,6 +43,7 @@ def String kusIP(String ns) {
 
 
 def checkPodRun(String gvr) {
+   
    def result = sh(script: "kubectl get " + gvr + "/${params.SERVICE_NAME} | awk 'NR >1 {print \$2}", returnStdout: true)
     // 1/1
     result = result.split("/")
@@ -52,7 +53,7 @@ def checkPodRun(String gvr) {
         info "恭喜你successful"
         sh "exit 0"
     }
-   println(gvr)
+   
 }
 
 
